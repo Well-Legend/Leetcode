7898b1c2b1aabe5ce72d2325ebd90e6db60dd881
Well-Legend <a0970378051@gmail.com>
2026-01-30 23:50:17 +0800

    change c++ to md

--- easy/169.Majority_Element.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public:
-        int majorityElement(vector<int>& nums) {
-            sort(nums.begin(), nums.end());
-            int ans = nums[nums.size()/2];
-            return ans;
-        }
-    };
